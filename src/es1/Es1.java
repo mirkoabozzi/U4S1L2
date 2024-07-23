e4844f0b61f13d69e365b3bedba539d759079d47
Mirko Abozzi <mirko.abozzi@gmail.com>
2024-07-23 13:51:26 +0200

    es1.1

--- src/es1/Es1.java
+++ src/es1/Es1.java
@@ -1,7 +1,25 @@
 package es1;
 
+import java.util.Scanner;
+
 public class Es1 {
     public static void main(String[] args) {
+
+    Scanner scanner= new Scanner(System.in);
+        System.out.println("inserisci la stringa");
+        String str= scanner.nextLine();
+        System.out.println(stringaPariDispari(str));
+
+
+
+
+    }
+
+    public static boolean stringaPariDispari(String str){
+
+        if (str.length() % 2 == 0) return true;
+        else return false;
+
 
     }
 }
